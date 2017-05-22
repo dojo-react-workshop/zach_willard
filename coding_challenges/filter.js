86e11496f7e84ca0a418fe81fb740cf724100925
Zach Willard <zacharywillard@northwesternmutual.com>
2017-05-22 13:42:52 -0500

    added JSFundamenalsPL

--- coding_challenges/filter.js
+++ coding_challenges/filter.js
@@ -1,3 +1,20 @@
-Array.prototype.filter = function () {
-    
+Array.prototype.filter = function (callback) {
+    const newArray = [];
+
+    this.forEach(function(value, index, array) {
+        if (callback(value, index, array)) {
+            newArray.push(value);
+        }
+    })
+
+    return newArray;
 }
+
+const a = [1,2,3];
+
+console.log(a.filter(function(value){
+    return (value % 2 == 0)
+}))
+
+// 167
+// 105-920-7419
--- coding_challenges/map.js
+++ coding_challenges/map.js
@@ -1,3 +1,15 @@
-Array.prototype.map = function () {
-    
+Array.prototype.map = function (callback) {
+    const newArray = [];
+
+    this.forEach(function(value, index, array) {
+        newArray.push(callback(value, index, array));
+    })
+
+    return newArray;
 }
+
+const a = [1,2,3];
+
+console.log(a.map(function(value){
+    return value * value;
+}))
